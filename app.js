f3f390baec1283a2297ca539f5ef70ab7dd70a84
samrawitgithubac <149234089+samrawitgithubac@users.noreply.github.com>
2024-08-24 05:54:47 -0700

    final

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const dotenv = require("dotenv").config();
 const express = require("express");
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const cors = require("cors");
 
 app.use(cors());
